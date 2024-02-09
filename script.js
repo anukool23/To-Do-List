018ce66a16b1eaf536b746542bd9afe2b0f725da
Anukool Patel <151703654+anukool23@users.noreply.github.com>
2024-02-09 23:43:17 +0530

    Added local storage

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 let form = document.querySelector("#todoForm")   //storing form element
 let tbody = document.querySelector("#tbody")    //storing table body element
-let data=[];
+let data=[];  //for storing the data of the To Do array 
 
+//function to load the data from local storage on the UI
+function loadData(){
+    let storedArr = JSON.parse(localStorage.getItem("todos"))
+    if(storedArr){  //To handle the user visiting for the first time
+        data = storedArr
+        showToDOData(data)
+    }
+}
+
+//function to save the data in local storage
+function saveData(){
+    localStorage.setItem("todos", JSON.stringify(data))
+}
 
 //Adding event listener to collect the form data on the click of submit
 form.addEventListener('submit', function(event){
@@ -21,7 +34,7 @@ function htmlData(event){
             status : false
         }
         data.push(obj)
-
+        saveData()
         showToDOData(data)
 }
 
@@ -62,14 +75,34 @@ function showToDOData(data){
         let td5=document.createElement('td')
         let delBtn = document.createElement("button")
         delBtn.innerHTML="Delete"
-        delBtn.addEventListener('click', function(){
-            data.splice(i, 1);  //For deleting data from 'data' array
-            tr.remove() //for removing row from HTML
+
+        delBtn.addEventListener('click', function() {
+            //Anyone of the below function can be use to delete the data from the table
+            handleDel(ele.id)
+            //handleDelete(i,tr)
         })
         td5.append(delBtn)
-
 
         tr.append(td1 , td2 , td3, td4, td5 )
        tbody.append(tr)
     })
 }
+
+// Function to delete data using filter HOF
+function handleDel(id){
+    data = data.filter(function(ele){
+        //We are filtering data which does not contain the data of specified ID and then replacing the array
+        return ele.id !== id;
+    })
+    saveData()
+    showToDOData(data)  //For updating the To Do table
+}
+
+//Function for deleting data using splice HOF
+function handleDelete(i,tr){
+    data.splice(i, 1);  //For deleting data from 'data' array
+    tr.remove() //for removing row from HTML
+    saveData()
+}
+
+loadData()
